e546569760646fc52e52259a938a113a819f27f7
Paweł Piontkowski <piontkowski35@gmail.com>
2021-03-25 09:29:19 +0100

    updating

--- mechanics.js
+++ mechanics.js
@@ -1,35 +1,91 @@
- const divFeature = [...document.querySelectorAll(".feature")];
- let mottoSection = document.querySelector(".motto_background");
-
 window.onload = function(){
-    const headerText = document.querySelector(".text");
-    headerText.style.left = 0;
+    const text = document.querySelector(".headerIn .text");
+    text.classList.add("activeHeader");
 }
 
-document.addEventListener("scroll", function(){
-    let $windowHeight = $(window).height()/2;
-    let $scrollValue = Math.floor($(document).scrollTop());
-    const $mottoHeight = Math.floor($(mottoSection).offset().top);
-    const $divHeight1 = Math.floor($(divFeature[0]).offset().top);
-    const $divHeight2 = Math.floor($(divFeature[1]).offset().top);
-    const $divHeight3 = Math.floor($(divFeature[2]).offset().top);
+document.addEventListener("scroll", ()=>{
 
-if($scrollValue > $divHeight1 - $windowHeight){
-    divFeature[0].style.opacity = 1;
+let scrollPoz = $(document).scrollTop();
+const windowHeight = $(window).height();
+
+const $feature1=$(".feature1");
+const $feature2=$(".feature2");
+const $feature3=$(".feature3");
+
+const feature1Height = $feature1.innerHeight();
+const feature2Height = $feature2.innerHeight();
+const feature3Height = $feature3.innerHeight();
+
+const feature1Poz = $feature1.offset().top;
+const feature2Poz = $feature2.offset().top;
+const feature3Poz = $feature3.offset().top;
+
+if(scrollPoz > feature1Poz + feature1Height - windowHeight*1.3){
+    $feature1.addClass("activeFeature");
 }
-if($scrollValue > $divHeight2 - $windowHeight){
-    divFeature[1].style.opacity = 1;
+if(scrollPoz > feature2Poz + feature2Height - windowHeight*1.3){
+    $feature2.addClass("activeFeature");
 }
-if($scrollValue > $divHeight3 - $windowHeight){
-    divFeature[2].style.opacity = 1;
-}
-if($scrollValue > $divHeight3 - $windowHeight){
-    divFeature[2].style.opacity = 1;
-}
-if($scrollValue > $mottoHeight - $windowHeight*1.3){
-    document.querySelector(".motto h1").style.left = 0;
-    document.querySelector("p.text").style.left = 0;
+if(scrollPoz > feature3Poz + feature3Height - windowHeight*1.3){
+    $feature3.addClass("activeFeature");
 }
 
-
+if(scrollPoz < 100){
+    $feature1.removeClass("activeFeature");
+    $feature2.removeClass("activeFeature");
+    $feature3.removeClass("activeFeature");
+}
 })
+// $(document).on("scroll", function(){
+//   const scrollValue = $(this).scrollTop();
+//   // console.log(scrollValue);
+
+// SEKCJA PIERWSZA
+
+//   const $art2 = $(".art2");
+//   const art2FromTop = $art2.offset().top;
+// const art2Height = $art2.outerHeight();
+
+//   const $art3 = $(".art3");
+//   const art3FromTop = $art3.offset().top;
+// const art3Height = $art3.outerHeight();
+
+// const $art4 = $(".art4");
+//   const art4FromTop = $art4.offset().top;
+// const art4Height = $art4.outerHeight();
+
+// SEKCJA DRUGA
+
+// const $op1 = $(".op1");
+// const $op2 = $(".op2");
+
+// const op1FromTop = $op1.offset().top;
+// const op2FromTop = $op2.offset().top;
+
+// const op1Height = $op1.height()/4;
+// const op2Height = $op2.height()/4;
+
+// if(scrollValue > op1FromTop + op1Height - windowHeigth){
+//   $op1.addClass("active");
+// }
+
+// if(scrollValue > op2FromTop + op2Height - windowHeigth){
+//   $op2.addClass("active");
+// }
+
+
+// if( scrollValue > art2FromTop +  art2Height - windowHeigth){
+//   $art2.addClass("active");
+// }
+// if( scrollValue > art3FromTop +  art3Height - windowHeigth){
+//   $art3.addClass("active");
+// }
+// if( scrollValue > art4FromTop +  art4Height - windowHeigth){
+//   $art4.addClass("active");
+// }
+
+// if( scrollValue < 100){
+//   $("article").removeClass("active");
+// }
+// })const windowHeigth = $(window).height();
+  
